fb66eb80b9b947d3b3a0419749437f29d872c418
Рэм Кудусов <rem.kudusov@gmail.com>
2023-09-21 22:35:24 +0300

    🎉 Initial commit.

---
+++ Configuration/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GithubCommitSaver.Configuration
+{
+#pragma warning disable CS1591 // Отсутствует комментарий XML для открытого видимого типа или члена
+    /// <summary>
+    /// Контекст web-приложения.
+    /// </summary>
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated(); // создать базу данных при первом обращении
+        }
+    }
+}
